ba5d96e39831955e5f3ab9aa736f9300fc16b11d
Neelesh <neeleshishu021@gmail.com>
2024-05-31 00:42:12 +0530

    final commit

--- src/components/superadmin/commoncomponents/navbar.js
+++ src/components/superadmin/commoncomponents/navbar.js
@@ -6,6 +6,7 @@ import Notification from './popup/notification';
 import Message from './popup/message';
 import Langague from './popup/langague';
 import ProfilePopup from './popup/profile';
+import './common.css';
 
 const Navbar = ({ toggleSidebar }) => {
   const [showNotification, setShowNotification] = useState(false);
@@ -36,21 +37,21 @@ const Navbar = ({ toggleSidebar }) => {
   };
 
   return (
-    <div className='flex justify-between p-4 bg-white rounded-lg m-2 shadow_card'>
+    <div className='flex justify-between p-4 bg-white rounded-lg m-2 shadow_card navbar_main_block'>
       <div className='flex gap-2 items-center cursor-pointer' onClick={handleProfileClick}>
         <img src='https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg' className='rounded-full h-10 w-10 object-cover' alt='Profile' />
         <p className='font-semibold'>Hii, Super Admin</p>
         <MdKeyboardArrowDown />
       </div>
 
-      <div className="relative w-1/2">
+      <div className="relative w-1/2 search_block">
         <input type="text" className="w-full py-3 pl-10 pr-4 bg-white border border-gray-300 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500" placeholder="Search..." />
         <svg className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35M18 11a7 7 0 11-14 0 7 7 0 0114 0z"></path>
         </svg>
       </div>
 
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center gap-2 buttons_navbar'>
         <div className='p-2 rounded-xl bg-cyan-300 cursor-pointer' onClick={handleNotificationClick}><CiBellOn className='text-cyan-800 text-xl' /></div>
         <div className='p-2 rounded-xl bg-cyan-300 cursor-pointer' onClick={handleMessageClick}><CiMail className='text-cyan-800 text-xl' /></div>
         <div className='p-2 rounded-xl bg-cyan-300 cursor-pointer' onClick={handleLanguageClick}><CiGlobe className='text-cyan-800 text-xl' /></div>
--- src/components/superadmin/sections/dashboard/dashboard.js
+++ src/components/superadmin/sections/dashboard/dashboard.js
@@ -7,103 +7,87 @@ import { GoPersonAdd } from "react-icons/go";
 import { RiBillLine } from "react-icons/ri";
 import { RiTodoLine } from "react-icons/ri";
 import { Chart as Chartjs } from 'chart.js/auto'
-import { Bar, Doughnut, Line, PolarArea } from 'react-chartjs-2'
+import { Bar, Doughnut, Line } from 'react-chartjs-2'
 import './dashboard.css';
 
 const Dashboard = () => {
-
     const data = [
-        {
-            "label": "ads",
-            "value": 200
-        },
-        {
-            "label": "subscription",
-            "value": 100
-        },
-        {
-            "label": "sponsership",
-            "value": 150
-        },
-        {
-            "label": "brand",
-            "value": 175
-        },
-    ]
+        { "label": "ads", "value": 200 },
+        { "label": "subscription", "value": 100 },
+        { "label": "sponsership", "value": 150 },
+        { "label": "brand", "value": 175 }
+    ];
 
     return (
-        
-            <div className=' flex flex-col gap-8 mt-4'>
-                <h1 className=' text-3xl font-semibold'>Dashboard</h1>
+        <div className='flex flex-col gap-8 mt-4'>
+            <h1 className='text-3xl font-semibold'>Dashboard</h1>
 
-                <div className=' grid grid-cols-3 gap-2 place-content-center'>
-                    <div className='dashboard_cards flex justify-between items-center grid1_dashboard p-4 rounded-xl'>
-                        <div className=' text-white'>
-                            <p className=' text-3xl font-semibold'>17</p>
-                            <p>Total User</p>
-                        </div>
-                        <div>
-                            <LuUsers2 className=' text-5xl text-white' />
-                        </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                <div className='dashboard_cards flex justify-between items-center grid1_dashboard p-4 rounded-xl'>
+                    <div className='text-white'>
+                        <p className='text-3xl font-semibold'>17</p>
+                        <p>Total User</p>
                     </div>
-
-                    <div className='dashboard_cards flex justify-between items-center grid2_dashboard p-4 rounded-xl'>
-                        <div className=' text-white'>
-                            <p className=' text-3xl font-semibold'>17</p>
-                            <p>Total User</p>
-                        </div>
-                        <div>
-                            <FiShoppingCart className=' text-5xl text-white' />
-                        </div>
+                    <div>
+                        <LuUsers2 className='text-5xl text-white' />
                     </div>
-
-                    <div className='dashboard_cards flex justify-between items-center grid3_dashboard p-4 rounded-xl'>
-                        <div className=' text-white'>
-                            <p className=' text-3xl font-semibold'>40</p>
-                            <p>Total Order</p>
-                        </div>
-                        <div>
-                            <TfiCup className=' text-5xl text-white' />
-                        </div>
-                    </div>
-
-                    <div className='dashboard_cards flex justify-between items-center grid4_dashboard p-4 rounded-xl'>
-                        <div className=' text-white'>
-                            <p className=' text-3xl font-semibold'>4</p>
-                            <p>Total Plans</p>
-                        </div>
-                        <div>
-                            <GoPersonAdd className=' text-5xl text-white' />
-                        </div>
-                    </div>
-
-                    <div className='dashboard_cards flex justify-between items-center grid5_dashboard p-4 rounded-xl'>
-                        <div className=' text-white'>
-                            <p className=' text-3xl font-semibold'>17</p>
-                            <p>Total User</p>
-                        </div>
-                        <div>
-                            <RiBillLine className=' text-5xl text-white' />
-                        </div>
-                    </div>
-
-                    <div className='dashboard_cards flex justify-between items-center grid6_dashboard p-4 rounded-xl'>
-                        <div className=' text-white'>
-                            <p className=' text-3xl font-semibold'>17</p>
-                            <p>About to Expire</p>
-                        </div>
-                        <div>
-                            <RiTodoLine className=' text-5xl text-white' />
-                        </div>
-                    </div>
-
-
                 </div>
 
-                <div className=' flex flex-col gap-4 bg-white rounded-2xl p-3'>
-                    <h1 className=' font-semibold text-2xl'>Recent Order</h1>
+                <div className='dashboard_cards flex justify-between items-center grid2_dashboard p-4 rounded-xl'>
+                    <div className='text-white'>
+                        <p className='text-3xl font-semibold'>17</p>
+                        <p>Total User</p>
+                    </div>
+                    <div>
+                        <FiShoppingCart className='text-5xl text-white' />
+                    </div>
+                </div>
 
-                    <Line
+                <div className='dashboard_cards flex justify-between items-center grid3_dashboard p-4 rounded-xl'>
+                    <div className='text-white'>
+                        <p className='text-3xl font-semibold'>40</p>
+                        <p>Total Order</p>
+                    </div>
+                    <div>
+                        <TfiCup className='text-5xl text-white' />
+                    </div>
+                </div>
+
+                <div className='dashboard_cards flex justify-between items-center grid4_dashboard p-4 rounded-xl'>
+                    <div className='text-white'>
+                        <p className='text-3xl font-semibold'>4</p>
+                        <p>Total Plans</p>
+                    </div>
+                    <div>
+                        <GoPersonAdd className='text-5xl text-white' />
+                    </div>
+                </div>
+
+                <div className='dashboard_cards flex justify-between items-center grid5_dashboard p-4 rounded-xl'>
+                    <div className='text-white'>
+                        <p className='text-3xl font-semibold'>17</p>
+                        <p>Total User</p>
+                    </div>
+                    <div>
+                        <RiBillLine className='text-5xl text-white' />
+                    </div>
+                </div>
+
+                <div className='dashboard_cards flex justify-between items-center grid6_dashboard p-4 rounded-xl'>
+                    <div className='text-white'>
+                        <p className='text-3xl font-semibold'>17</p>
+                        <p>About to Expire</p>
+                    </div>
+                    <div>
+                        <RiTodoLine className='text-5xl text-white' />
+                    </div>
+                </div>
+            </div>
+
+            <div className='flex flex-col gap-4 bg-white rounded-2xl p-3'>
+                <h1 className='font-semibold text-2xl'>Recent Order</h1>
+
+                <Line
                     data={{
                         labels: data.map((item) => item.label),
                         datasets: [
@@ -125,61 +109,59 @@ const Dashboard = () => {
                         },
                     }}
                 />
-                </div>
-
-                <div className=' flex items-center gap-2 w-full'>
-                    <div className='bg-white rounded-2xl p-3 flex flex-col gap-3'>
-                    <h1 className=' font-semibold text-2xl'>Subscriptions</h1>
-                    <Doughnut
-                            data={{
-                                labels: data.map((item) => item.label),
-                                datasets: [
-                                    {
-                                        label: "counts",
-                                        borderRadius: 5,
-                                        data: data.map((item) => item.value),
-                                        backgroundColor: ["#7A7AFF", "#ffdd33", "#ff9b2a", "#4e5fcc"],
-                                    }
-                                ],
-                            }}
-                            options={{
-                                tension: 0.4,
-                                plugins: {
-                                    legend: {
-                                        display: true,
-                                    },
-                                },
-                            }}
-                        />
-                    </div>
-                    <div className='bg-white rounded-2xl p-3 flex flex-col gap-3 w-full'>
-                    <h1 className=' font-semibold text-2xl'>Monthly Orders</h1>
-                    <Bar
-                            data={{
-                                labels: data.map((item) => item.label),
-                                datasets: [
-                                    {
-                                        label: "counts",
-                                        borderRadius: 5,
-                                        data: data.map((item) => item.value),
-                                        backgroundColor: ["#7A7AFF", "#ffdd33", "#ff9b2a", "#4e5fcc"],
-                                    }
-                                ],
-                            }}
-                            options={{
-                                tension: 0.4,
-                                plugins: {
-                                    legend: {
-                                        display: true,
-                                    },
-                                },
-                            }}
-                        />
-                    </div>
-                </div>
-                
-
             </div>
+
+            <div className='flex flex-col lg:flex-row items-center gap-4 w-full'>
+                <div className='bg-white rounded-2xl p-3 flex flex-col gap-3 w-full lg:w-1/2'>
+                    <h1 className='font-semibold text-2xl'>Subscriptions</h1>
+                    <Doughnut
+                        data={{
+                            labels: data.map((item) => item.label),
+                            datasets: [
+                                {
+                                    label: "counts",
+                                    borderRadius: 5,
+                                    data: data.map((item) => item.value),
+                                    backgroundColor: ["#7A7AFF", "#ffdd33", "#ff9b2a", "#4e5fcc"],
+                                }
+                            ],
+                        }}
+                        options={{
+                            tension: 0.4,
+                            plugins: {
+                                legend: {
+                                    display: true,
+                                },
+                            },
+                        }}
+                    />
+                </div>
+                <div className='bg-white rounded-2xl p-3 flex flex-col gap-3 w-full lg:w-1/2'>
+                    <h1 className='font-semibold text-2xl'>Monthly Orders</h1>
+                    <Bar
+                        data={{
+                            labels: data.map((item) => item.label),
+                            datasets: [
+                                {
+                                    label: "counts",
+                                    borderRadius: 5,
+                                    data: data.map((item) => item.value),
+                                    backgroundColor: ["#7A7AFF", "#ffdd33", "#ff9b2a", "#4e5fcc"],
+                                }
+                            ],
+                        }}
+                        options={{
+                            tension: 0.4,
+                            plugins: {
+                                legend: {
+                                    display: true,
+                                },
+                            },
+                        }}
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 
